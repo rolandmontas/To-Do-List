4dfed2455c6172cb88bbc4452dcf26248ebd5d66
Rolando Montas <roland@rolandos-macbook-pro.local>
2018-11-17 07:45:17 -0600

    To Do List complete with no datasource methods.

---
+++ Todoey/TodoListViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Rolando Montas on 11/17/18.
+//  Copyright © 2018 Rolando Montas. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    
+    let itemArray = ["Find Mike", "Buy Eggos", "Destory Demogorgon"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    //Mark - Tableview Datasource Methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    //Mark - TableView Delegate Methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        print(itemArray[indexPath.row])
+        
+        //This if statement check if the current selected item has a checkmark if so change it to none
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        //This will make the selected cell to flash as its selected color and go back to normal.
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+    
+}
+
